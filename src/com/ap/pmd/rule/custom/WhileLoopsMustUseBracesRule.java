e64061b7891de46cea87c93c22cb38c52d13f947
mtang-at-secura <Mec_Tang@secura.net>
2018-09-06 18:08:25 +0800

    finish the Java-based custom rule example

--- src/com/ap/pmd/rule/custom/WhileLoopsMustUseBracesRule.java
+++ src/com/ap/pmd/rule/custom/WhileLoopsMustUseBracesRule.java
@@ -1,5 +1,7 @@
 package com.ap.pmd.rule.custom;
 
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTBlock;
 import net.sourceforge.pmd.lang.java.ast.ASTWhileStatement;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
@@ -8,10 +10,16 @@ public class WhileLoopsMustUseBracesRule extends AbstractJavaRule {
 
 	@Override
 	public Object visit(ASTWhileStatement node, Object data) {
-		System.out.println("Hello, World!");
-		return data;
+		Node firstStmt = node.jjtGetChild(1);
+		if(!hasBlockAsFirstChild(firstStmt)) {
+			addViolation(data, node);
+		}
+		return super.visit(node, data);
 	}
 
-	
+	private boolean hasBlockAsFirstChild(Node node) {
+		return 0 != node.jjtGetNumChildren() &&
+				(node.jjtGetChild(0) instanceof ASTBlock);
+	}
 
 }
