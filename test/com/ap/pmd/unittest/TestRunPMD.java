dc108331096f274663b5bba107af5d11e59c27ad
mtang-at-secura <Mec_Tang@secura.net>
2018-09-06 16:00:29 +0800

    prefer the -no-cache way to run the PMD report within script

--- test/com/ap/pmd/unittest/TestRunPMD.java
+++ test/com/ap/pmd/unittest/TestRunPMD.java
@@ -1,6 +1,7 @@
 package com.ap.pmd.unittest;
 
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import net.sourceforge.pmd.PMD;
@@ -13,6 +14,14 @@ public class TestRunPMD {
 	@Test
 	public void testRunPMDAgainstSrc() {
 		String[] args = "-d ./src/ -R rules/sample-ruleset.xml -auxclasspath ./bin/ -cache pmd-cache -f text".split(WHITE_SPACE);
+		//PMD.main(args);	//System.exit() will result into JUnit Test runner yielding a gray bar
+		PMD.run(args);
+	}
+	
+	@Ignore("Run PMD with -no-cache")
+	@Test
+	public void testRunPMDAgainstSrcWithoutCache() {
+		String[] args = "-d ./src/ -R rules/sample-ruleset.xml -auxclasspath ./bin/ -no-cache -f text".split(WHITE_SPACE);
 		//PMD.main(args);	//System.exit() will result into JUnit Test runner yielding a gray bar
 		PMD.run(args);
 	}
