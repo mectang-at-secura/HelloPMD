7da7c944e2d38e3f1718f5c924910eef9a322bd4
mtang-at-secura <Mec_Tang@secura.net>
2018-09-18 17:06:23 +0800

    correct the CMD script file, as well as remove redundant sample files

--- rules/xslt/saxon-cmd-transform-xml.cmd
+++ rules/xslt/saxon-cmd-transform-xml.cmd
@@ -1,6 +1,6 @@
 rem 2018-08-30
 rem Saxon commandline refrence: http://www.saxonica.com/documentation/index.html#!using-xsl/commandline
-SAXON_JAR_PATH={SAXON_JAR_FILE_PATH}
-XML_FILE_PATH={XML_FILE_PATH}
-XSL_FILE_PATH=tdf-extract-attr-of-fields.xsl
+set SAXON_JAR_PATH={SAXON_JAR_FILE_PATH}
+set XML_FILE_PATH={XML_FILE_PATH}
+set XSL_FILE_PATH=tdf-extract-attr-of-fields.xsl
 java -jar %SAXON_JAR_PATH% -s:"%XML_FILE_PATH%" -xsl:%XSL_FILE_PATH%
